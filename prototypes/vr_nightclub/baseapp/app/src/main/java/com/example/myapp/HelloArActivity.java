5467ced006f6a7abcdd408fa8fba47c26c1726d8
MasterHansCoding <loicgirard75@gmail.com>
2024-08-25 19:08:41 +0200

    Added SoundUtils class

--- prototypes/vr_nightclub/baseapp/app/src/main/java/com/example/myapp/HelloArActivity.java
+++ prototypes/vr_nightclub/baseapp/app/src/main/java/com/example/myapp/HelloArActivity.java
@@ -37,6 +37,8 @@
 
 package com.example.myapp;
 
+import static com.mobilevr.sound.SoundUtils.processInputVolume;
+import static com.mobilevr.sound.SoundUtils.processOrientation;
 import static com.mobilevr.utils.GeometryUtils.buildPerspectiveMatrix;
 import static com.mobilevr.utils.GeometryUtils.calculateAspectRatio;
 import static com.mobilevr.utils.GeometryUtils.createPerspectiveMatrix;
@@ -763,78 +765,6 @@ public class HelloArActivity extends AppCompatActivity implements SampleRender.R
       //session.setCameraConfig(cameraConfig);
       session.configure(config);
     }
-
-  /**
-   * Returns the azimut of the listener
-   *
-   * @param soundPos
-   * @param listenerPos
-   * @param camQuat
-   * @return float: azimut
-   */
-  private float processOrientation(float[] soundPos, float[] listenerPos, float[] camQuat) {
-    float azimut;
-
-    // Calculate the relative position
-    float[] relPos = new float[] {
-            - soundPos[0] + listenerPos[0],
-            soundPos[1] - listenerPos[1],
-            soundPos[2] - listenerPos[2],
-            1
-    };
-
-    // Extract rotation around Y axis from camera quaternion
-    float[] camRotationMatrix = quaternionToMatrix(camQuat);
-    float[] yRotationMatrix = new float[] {
-            camRotationMatrix[0], 0, camRotationMatrix[2], 0,
-            0, 1, 0, 0,
-            camRotationMatrix[8], 0, camRotationMatrix[10], 0,
-            0, 0, 0, 1
-    };
-
-    // Apply Y rotation to the relPos vector
-    float[] relPosRotated = new float[4];
-    Matrix.multiplyMV(relPosRotated, 0, yRotationMatrix, 0, relPos, 0);
-
-    azimut = (float) Math.toDegrees( Math.atan2(relPosRotated[0], relPosRotated[3]) );
-
-    azimut = ( azimut + 360 ) % 360;
-
-    // Reverse rotation
-    azimut = 360 - azimut;
-
-    return azimut;
-  }
-
-  /**
-   * Returns the input volume from 0 to 1.
-   *
-   * @param soundPos
-   * @param listenerPos
-   * @param maxHearingDistance
-   * @return
-   */
-  private float processInputVolume(float[] soundPos,
-                                   float[] listenerPos,
-                                   float maxHearingDistance) {
-    float inputVolume;
-
-    // Calculate the relative position
-    float[] relPos = new float[] {
-            soundPos[0] - listenerPos[0],
-            soundPos[1] - listenerPos[1],
-            soundPos[2] - listenerPos[2]
-    };
-    float distance = VectorUtils.norm(relPos);
-
-    // ratio distance from sound to listener / max hearing distance
-    inputVolume = - distance / maxHearingDistance + 1;
-    if (distance >= maxHearingDistance) {
-      return 0;
-    } else {
-      return inputVolume;
-    }
-  }
 
   // Native
   private native void NativeInit(int samplerate, int buffersize, String tempPath);
